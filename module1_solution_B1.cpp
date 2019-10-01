e0c6bc44e5e71527ca776d9d40c10efc4d39854b
VladimirBalditsyn <48633877+VladimirBalditsyn@users.noreply.github.com>
2019-10-01 19:41:34 +0300

    Update module1_solution_B1.cpp

--- module1_solution_B1.cpp
+++ module1_solution_B1.cpp
@@ -4,35 +4,37 @@
 #include <vector>
 #include <bitset>
 
-template <int T>
-int most_significant_bit(std::bitset<T>& set) {
+template <size_t T>
+size_t least_significant_bit(std::bitset<T>& set) {
     for (int k = 1; k < T; ++k){
         if (set[k] != false){
             return k;
         }
     }
-    return 0;
+    return T;
 }
 
-std::string make_string(const std::vector<int>& pref_func) {
-    std::string answer = "a";
-    constexpr int alphabet_size = 26;
+std::string make_string(const std::vector<size_t >& pref_func) {
+    std::string answer;
+    constexpr size_t alphabet_size = 26;
+    constexpr char first_letter = 'a';
+    answer += first_letter;
     std::bitset<alphabet_size> alphabet;
     alphabet.set();
-    for (int i = 1; i < pref_func.size(); ++i) {
+    for (size_t i = 1; i < pref_func.size(); ++i) {
         if (pref_func[i] == 0) {
-            int prev_pref_func = pref_func[i - 1]; //значение префикс-функции для символа, стоящего перед
+            size_t prev_pref_func = pref_func[i - 1]; //значение префикс-функции для символа, стоящего перед
             // рассматриваемым
             alphabet[0] = false; //при префик-функции равной 0 новый символ не 'a'
 
             while (prev_pref_func != 0) {
                 //проверяем, что не продлеваем  префикс меньшей длины
-                alphabet[answer[prev_pref_func] - 'a'] = false;
+                alphabet[answer[prev_pref_func] - first_letter] = false;
                 prev_pref_func = pref_func[prev_pref_func - 1];
             }
 
-            int left_letter = most_significant_bit<alphabet_size>(alphabet);
-            answer += static_cast<char>(left_letter + 'a');
+            size_t left_letter = least_significant_bit<alphabet_size>(alphabet);
+            answer += static_cast<char>(left_letter + first_letter);
 
             alphabet.set();
             continue;
@@ -43,8 +45,8 @@ std::string make_string(const std::vector<int>& pref_func) {
 }
 
 int main() {
-    std::vector<int> input;
-    int c = 0;
+    std::vector<size_t > input;
+    size_t c = 0;
     while(std::cin >> c) {
         input.push_back(c);
     }
