7a602cdb0c25bfc3739269896e6605ceedbb8612
VladimirBalditsyn <48633877+VladimirBalditsyn@users.noreply.github.com>
2019-10-06 22:15:58 +0300

    Косметические исправления

--- module1_solution_B1.cpp
+++ module1_solution_B1.cpp
@@ -6,7 +6,7 @@
 
 template <size_t T>
 size_t least_significant_bit(std::bitset<T>& set) {
-    for (int k = 1; k < T; ++k){
+    for (size_t k = 1; k < T; ++k) {
         if (set[k] != false){
             return k;
         }
@@ -14,7 +14,7 @@ size_t least_significant_bit(std::bitset<T>& set) {
     return T;
 }
 
-std::string make_string(const std::vector<size_t >& pref_func) {
+std::string restore_from_prefix_func(const std::vector<size_t>& pref_func) {
     std::string answer;
     constexpr size_t alphabet_size = 26;
     constexpr char first_letter = 'a';
@@ -22,24 +22,24 @@ std::string make_string(const std::vector<size_t >& pref_func) {
     std::bitset<alphabet_size> alphabet;
     alphabet.set();
     for (size_t i = 1; i < pref_func.size(); ++i) {
-        if (pref_func[i] == 0) {
-            size_t prev_pref_func = pref_func[i - 1]; //значение префикс-функции для символа, стоящего перед
-            // рассматриваемым
-            alphabet[0] = false; //при префик-функции равной 0 новый символ не 'a'
-
-            while (prev_pref_func != 0) {
-                //проверяем, что не продлеваем  префикс меньшей длины
-                alphabet[answer[prev_pref_func] - first_letter] = false;
-                prev_pref_func = pref_func[prev_pref_func - 1];
-            }
-
-            size_t left_letter = least_significant_bit<alphabet_size>(alphabet);
-            answer += static_cast<char>(left_letter + first_letter);
-
-            alphabet.set();
+        if (pref_func[i] != 0) {
+            answer += static_cast<char>(answer[pref_func[i] - 1]);
             continue;
         }
-        answer += static_cast<char>(answer[pref_func[i] - 1]);
+        size_t prev_pref_func = pref_func[i - 1]; //значение префикс-функции для символа, стоящего перед
+        // рассматриваемым
+        alphabet[0] = false; //при префик-функции равной 0 новый символ не 'a'
+
+        while (prev_pref_func != 0) {
+            //проверяем, что не продлеваем  префикс меньшей длины
+            alphabet[answer[prev_pref_func] - first_letter] = false;
+            prev_pref_func = pref_func[prev_pref_func - 1];
+        }
+
+        size_t left_letter = least_significant_bit(alphabet);
+        answer += static_cast<char>(left_letter + first_letter);
+
+        alphabet.set();
     }
     return answer;
 }
@@ -50,7 +50,7 @@ int main() {
     while(std::cin >> c) {
         input.push_back(c);
     }
-    std::string answer = make_string(input);
+    std::string answer = restore_from_prefix_func(input);
     std::cout << answer;
     return 0;
 }
